79eff1adad1179309f30946e822bcaf7ce7582c1
Jugal Verma <joogal89@gmail.com>
2026-02-03 01:40:56 +0530

    Code change with Backebd
    Updated azurerm provider version and added backend configuration.

--- providers.tf
+++ providers.tf
@@ -1,13 +1,20 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.58.0"
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "4.57.0"
+        }
     }
-  }
+    backend "azurerm" {
+      resource_group_name = "rock_backend-Jugal"
+      storage_account_name = "rockbackend1"
+      container_name = "tfstate"
+      key = "dev.terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "4f745e59-7394-4e4f-be89-96b2457d289c"
+
 }
